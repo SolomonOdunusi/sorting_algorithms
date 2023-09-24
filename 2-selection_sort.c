323dc1381053dbea794b78404eccc84ad02ac889
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-24 16:52:01 -0700

    debug selection sort file

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,18 +15,23 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i; j < size; j++)
+
+		for (j = i + 1; j < size; j++)
+		{
 			if (array[j] < array[min])
 				min = j;
-			if (min != i)
-			{
-				temp = array[i];
-				array[i] = array[min];
-				array[min] = temp;
-				print_array(array, size);
-			}
+		}
+
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 	}
 }
