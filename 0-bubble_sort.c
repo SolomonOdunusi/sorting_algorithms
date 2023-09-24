ac6c13b758a5132e0a0e89552e67599d936ac48f
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-24 16:36:28 -0700

    Fixed some bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
+	int i, j, temp;
 	bool swapped;
 
 	if (array == NULL || size < 2)
@@ -21,7 +21,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
 				swapped = true;
 				print_array(array, size);
 			}
